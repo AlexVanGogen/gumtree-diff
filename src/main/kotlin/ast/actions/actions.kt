50f4c3d089dcf25011f3e0983e4d9902fa14f644
alexvangogen <alexvangogen@gmail.com>
2019-09-11 12:57:58 +0300

    Add unique ids to move and update modifications
    It will help to match moved/updated nodes from both trees by looking at output.

--- src/main/kotlin/ast/actions/actions.kt
+++ src/main/kotlin/ast/actions/actions.kt
@@ -6,9 +6,12 @@ import com.github.gumtreediff.tree.ITree
 sealed class ModificationKind(open val node: ITree) {
 
     companion object {
+        private var lastModeId = 0
+        private var lastUpdateId = 0
+
         fun from(action: Action): ModificationKind = when (action) {
-            is Update -> UpdateModification(action.node, action.node.label, action.value)
-            is Move -> MoveModification(action.node)
+            is Update -> UpdateModification(action.node, action.node.label, action.value, lastUpdateId++)
+            is Move -> MoveModification(action.node, lastModeId++)
             is Insert -> InsertModification(action.node)
             is Delete -> DeleteModification(action.node)
             else -> UnknownModification(action.node)
@@ -16,12 +19,12 @@ sealed class ModificationKind(open val node: ITree) {
     }
 }
 
-class UpdateModification(node: ITree, val from: String, val to: String) : ModificationKind(node) {
-    override fun toString() = "updated: \"$from\" -> \"$to\""
+class UpdateModification(node: ITree, val from: String, val to: String, val id: Int) : ModificationKind(node) {
+    override fun toString() = "updated: \"$from\" -> \"$to\" | #$id"
 }
 
-class MoveModification(node: ITree) : ModificationKind(node) {
-    override fun toString() = "moved"
+class MoveModification(node: ITree, val id: Int) : ModificationKind(node) {
+    override fun toString() = "moved | #$id"
 }
 
 class InsertModification(node: ITree) : ModificationKind(node) {
